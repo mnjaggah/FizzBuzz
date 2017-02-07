c6288ab1ed159d8c9c2b477add62f6325286bcc4
Maria <maria.njaggah@gmail.com>
2017-02-07 15:29:11 +0300

    first commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,21 +1,9 @@
-def data_type(n):
-  if isinstance(n, str):
-    return len(n)
-  elif isinstance(n, bool):
-    return n
-  elif n is None:
-    return 'no value'
-  elif isinstance(n, int):
-    if n < 100:
-      return 'less than 100'
-    elif n == 100:
-      return 'equal to 100'
-    else:
-      return 'more than 100'
-  elif isinstance(n, list):
-    if len(n) <= 2:
-      return None
-    else:
-      return n[2]
+def fizz_buzz(n):
+  if n % 3 == 0 and n % 5 == 0:
+    return "FizzBuzz"
+  elif n % 3 == 0:
+    return "Fizz"
+  elif n % 5 == 0:
+    return "Buzz"
   else:
-    None
+    return n
